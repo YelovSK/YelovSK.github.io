fcbe5b882537af1340c45620673c703bb2ee4024
YelovSK <hampatrik@gmail.com>
2025-04-12 14:57:18 +0200

    hash location strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,13 @@ import { ApplicationConfig } from '@angular/core';
 import { routes } from './app-routing.module';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
-        provideHttpClient()
+        provideHttpClient(),
+        { provide: LocationStrategy, useClass: HashLocationStrategy }
     ]
 };
